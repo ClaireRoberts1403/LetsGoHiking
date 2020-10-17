ca54188ef888d98f66dcfa1d352f4f000fef3824
ClaireRoberts1403 <claireroberts1403@gmail.com>
2020-10-17 12:55:24 +0000

    setup local and production database setting

--- LetsGoHiking/settings.py
+++ LetsGoHiking/settings.py
@@ -77,12 +77,17 @@ WSGI_APPLICATION = 'LetsGoHiking.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database.url.parse(os.environ.get('DATABASE_URL'))
+        }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
     }
-}
 
 
 # Password validation
