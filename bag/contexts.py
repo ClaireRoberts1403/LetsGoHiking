a704af313e2a97d9cddfea741eae889d3f1c8d3b
ClaireRoberts1403 <claireroberts1403@gmail.com>
2020-10-26 14:07:43 +0000

    grand total and item in cart counter functioning correctly

--- bag/contexts.py
+++ bag/contexts.py
@@ -12,6 +12,7 @@ def bag_contents(request):
 
     for item_id, item_data in bag.items():
         product = get_object_or_404(Product, pk=item_id)
+        total += item_data * product.product_price
         product_count += item_data
         bag_items.append({
             'item_id': item_id,
