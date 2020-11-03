58caf79842e337934b9c46514b1b38ed67f36718
ClaireRoberts1403 <claireroberts1403@gmail.com>
2020-11-03 15:35:56 +0000

    stripe card element added

--- payment/views.py
+++ payment/views.py
@@ -15,6 +15,8 @@ def payment(request):
     template = 'payment/payment.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51HZDumBr9rDAn4pHhR9viH3NbBYSif3xmLDYkpWkP3trKnCPiP9ZnhwuXpZTjJVau7qRB7bIIy8H1PhlAShE06fu00OEmLsz6Q',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
